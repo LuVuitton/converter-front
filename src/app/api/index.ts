f766f0fdd1fb67132175daa58ea3c6bd91be62ad
FelixT <felixtekhada@gmail.com>
2023-12-25 02:46:28 +0100

    added url from render

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,5 +1,5 @@
 export const RATES_TAG = "rates";
 export const PREV_RATES_TAG = "prev_rates";
 export const BASE_URL_RATES = "https://api.currencybeacon.com/v1";
-export const BASE_URL_MAIN = "http://localhost:3000";
+export const BASE_URL_MAIN = "https://converter-back.onrender.com";
 
